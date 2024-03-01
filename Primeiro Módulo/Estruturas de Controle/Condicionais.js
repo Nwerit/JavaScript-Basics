f0a7295df81dcdcbc5ea1ef53f30672601764b3b
Marcus <Mavitos23@gmail.com>
2024-02-29 22:04:28 -0300

    commit: condicionais

--- Primeiro Módulo/Estruturas de Controle/Condicionais.js
+++ Primeiro Módulo/Estruturas de Controle/Condicionais.js
@@ -28,3 +28,34 @@
     } 
 }
 
+// O terceiro tipo de Estrutura de controle que temos, chama se 'SWITCH'. Normalmente é utilizada quando há multiplas condições a serem avaliadas em relação a uma ÚNICA EXPRESSÃO ela compara a expressão especificada dos 'CASE' e executa o bloco de código correspondente ao primeiro 'CASE' que corresponder.
+
+
+{
+    let diaSemana = 7;
+    switch(diaSemana){
+        case 1:
+            console.log("Segunda-Feira!");
+            break
+        case 2:
+            console.log("Terça-Feira!");
+            break
+        case 3:
+            console.log("Quarta-feira!");
+            break   
+        case 4: 
+            console.log("Quinta-feira!");
+            break
+        case 5:
+            console.log("Sexta-Feira!");
+            break
+        case 6:
+            console.log("Sábado!");
+            break
+        case 7:
+            console.log("Domingo!");
+            break
+        default:
+            console.log("Dia inválido");                     
+    }
+}
